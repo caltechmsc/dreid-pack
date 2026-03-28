dffb8dcf0960fa0e87075e53afbe6d99db35197c
Tony Kan <tonykan@caltech.edu>
2026-03-28 01:54:57 -0700

    feat(lib): Expose public API via re-exports at crate root

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+mod model;
+mod pack;
+
 pub mod io;
-pub mod model;
-pub mod pack;
+
+pub use model::residue::ResidueType;
+pub use model::system::{
+    BuckMatrix, BuckPair, FixedAtomPool, ForceFieldParams, HBondParams, LjMatrix, LjPair, Residue,
+    SidechainAtoms, System, VdwMatrix,
+};
+pub use model::types::{TypeIdx, Vec3};
+
+pub use pack::PackConfig;
+pub use pack::pack;
